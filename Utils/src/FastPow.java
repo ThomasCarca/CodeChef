79580b872fba1652538ce827e2bdd403585e1909
tboulbes <boulbestho@eisti.eu>
2017-05-16 15:50:37 +0100

    Add beginner projects + modification comments

--- Utils/src/FastPow.java
+++ Utils/src/FastPow.java
@@ -1,4 +1,16 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FastPow.
+ */
 public class FastPow {
+	
+	/**
+	 * Pow.
+	 *
+	 * @param a the a
+	 * @param b the b
+	 * @return the long
+	 */
 	public static long pow(long a, long b) {
 		long re = 1;
 		while (b > 0) {
