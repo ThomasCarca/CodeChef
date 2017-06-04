c184d05c8fa4d2de53ebb8eed9dd9eb081badfa7
Administrator <boulbestho@eisti.eu>
2017-06-05 01:13:26 +0200

    NEO01 - Answer Accepted

---
+++ Compete/JuneChallenge2017/NEO01/src/Main.java
@@ -0,0 +1,147 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		FastReader rdr = new FastReader();
+		PrintWriter stdout = new PrintWriter(System.out);
+		int T = rdr.nextInt();
+		while (T-- > 0) {
+			int n = rdr.nextInt();
+			long subset = 0, countSubset = 0, total = 0, subsetTotal = 0;
+			List<Long> negatives = new ArrayList<Long>();
+			while (n-- > 0) {
+				long currentNumber = rdr.nextLong();
+				if (currentNumber >= 0) {
+					subset += currentNumber;
+					countSubset += 1;
+				} else {
+					negatives.add(currentNumber);
+				}
+			}
+			Collections.sort(negatives);
+			Collections.reverse(negatives);
+			subsetTotal = subset*countSubset;
+			for (Long negative : negatives) {
+				long negativeInSubset = (subset+negative)*(countSubset+1);
+				if (subsetTotal < negativeInSubset) {
+					subsetTotal = negativeInSubset;
+					subset += negative;
+					countSubset++;
+				} else {
+					total += negative;
+				}
+			}
+			stdout.println(total + subsetTotal);
+		}
+		stdout.flush();
+		stdout.close();
+	}
+
+	//////////////////////////////////////////////////
+	////////////////// +--------+ ////////////////////
+	////////////////// | READER | ////////////////////
+	////////////////// +--------+ ////////////////////
+	//////////////////////////////////////////////////
+
+	/**
+	 * The Class FastReader.
+	 */
+	static class FastReader {
+
+		/** The br. */
+		BufferedReader br;
+
+		/** The st. */
+		StringTokenizer st;
+
+		/**
+		 * Instantiates a new fast reader.
+		 */
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		/**
+		 * Next.
+		 *
+		 * @return the string
+		 */
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		/**
+		 * Next int.
+		 *
+		 * @return the int
+		 */
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		/**
+		 * Next long.
+		 *
+		 * @return the long
+		 */
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		/**
+		 * Next double.
+		 *
+		 * @return the double
+		 */
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		/**
+		 * Next line.
+		 *
+		 * @return the string
+		 */
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
