246a78b7cc224ae644d74329e4841b40eac479d0
tboulbes <boulbestho@eisti.eu>
2017-06-07 18:09:00 +0100

    ES - In progress

---
+++ Compete/JuneChallenge2017/ES/src/Main.java
@@ -0,0 +1,131 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.StringTokenizer;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	public static final BigDecimal E = new BigDecimal(Math.exp(1));
+
+	public static BigDecimal S(BigDecimal val, BigDecimal n) {
+		if (n.compareTo(new BigDecimal("0")) == 0) {
+			return new BigDecimal("0");
+		} else {
+			BigDecimal m = (n.multiply(val.subtract(new BigDecimal("1")))).setScale(0,  RoundingMode.FLOOR);
+			return (m.add(n)).multiply((m.add(n).add(new BigDecimal("1"))).divide(new BigDecimal("2"), 2, RoundingMode.HALF_UP).subtract(S(val.divide(val.subtract(new BigDecimal("1"))), m)));
+		}
+
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		FastReader rdr = new FastReader();
+		PrintWriter stdout = new PrintWriter(System.out);
+		BigDecimal n = new BigDecimal(rdr.nextLine());
+		stdout.println(S(E,n));
+		stdout.flush();
+		stdout.close();
+	}
+
+	//////////////////////////////////////////////////
+	////////////////// +--------+ ////////////////////
+	////////////////// | READER | ////////////////////
+	////////////////// +--------+ ////////////////////
+	//////////////////////////////////////////////////
+
+	/**
+	 * The Class FastReader.
+	 */
+	static class FastReader {
+
+		/** The br. */
+		BufferedReader br;
+
+		/** The st. */
+		StringTokenizer st;
+
+		/**
+		 * Instantiates a new fast reader.
+		 */
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		/**
+		 * Next.
+		 *
+		 * @return the string
+		 */
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		/**
+		 * Next int.
+		 *
+		 * @return the int
+		 */
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		/**
+		 * Next long.
+		 *
+		 * @return the long
+		 */
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		/**
+		 * Next double.
+		 *
+		 * @return the double
+		 */
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		/**
+		 * Next line.
+		 *
+		 * @return the string
+		 */
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
