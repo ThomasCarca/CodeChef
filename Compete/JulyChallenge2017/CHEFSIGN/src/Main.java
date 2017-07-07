6b45c175b521f9a74a2bdb5a09ac0f513561c1b6
tboulbes <boulbestho@eisti.eu>
2017-07-07 16:53:19 +0100

    CHEFSIGN - Wrong Answer

---
+++ Compete/JulyChallenge2017/CHEFSIGN/src/Main.java
@@ -0,0 +1,141 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	protected static int solve(String signs) {
+		char[] splitted = signs.toCharArray();
+		int count = 1, min = 1, max = 1;
+		for (int i = 0; i < splitted.length; i++) {
+			char sign = splitted[i];
+			if (sign == '<') {
+				count++;
+			} else if (sign == '>') {
+				count--;
+			}
+			if (count > max) {
+				max = count;
+			}
+			if (count < min) {
+				min = count;
+			}
+		}
+		count = max - min + 1;
+		return count;
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		FastReader rdr = new FastReader();
+		PrintWriter stdout = new PrintWriter(System.out);
+		int T = rdr.nextInt();
+		while (T-- > 0) {
+			String signs = rdr.nextLine();
+			stdout.println(solve(signs));
+		}
+		stdout.flush();
+		stdout.close();
+	}
+
+	//////////////////////////////////////////////////
+	////////////////// +--------+ ////////////////////
+	////////////////// | READER | ////////////////////
+	////////////////// +--------+ ////////////////////
+	//////////////////////////////////////////////////
+
+	/**
+	 * The Class FastReader.
+	 */
+	static class FastReader {
+
+		/** The br. */
+		BufferedReader br;
+
+		/** The st. */
+		StringTokenizer st;
+
+		/**
+		 * Instantiates a new fast reader.
+		 */
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		/**
+		 * Next.
+		 *
+		 * @return the string
+		 */
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		/**
+		 * Next int.
+		 *
+		 * @return the int
+		 */
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		/**
+		 * Next long.
+		 *
+		 * @return the long
+		 */
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		/**
+		 * Next double.
+		 *
+		 * @return the double
+		 */
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		/**
+		 * Next line.
+		 *
+		 * @return the string
+		 */
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
---
+++ Compete/JulyChallenge2017/CHEFSIGN/src/MainTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		assertEquals(1, Main.solve("="));
+		assertEquals(1, Main.solve("=="));
+		assertEquals(2, Main.solve("<"));
+		assertEquals(2, Main.solve(">"));
+		assertEquals(2, Main.solve("<>"));
+		assertEquals(2, Main.solve("><"));
+		assertEquals(2, Main.solve("<="));
+		assertEquals(2, Main.solve("=<"));
+		assertEquals(2, Main.solve(">="));
+		assertEquals(2, Main.solve("=>"));
+		assertEquals(2, Main.solve("<>"));
+		assertEquals(2, Main.solve("><"));
+		assertEquals(2, Main.solve("==>="));
+		assertEquals(3, Main.solve("<<"));
+		assertEquals(3, Main.solve("<<>>"));
+		assertEquals(3, Main.solve(">><<"));
+		assertEquals(3, Main.solve("<>>"));
+		assertEquals(3, Main.solve("><>>"));
+		assertEquals(4, Main.solve("<>>>"));
+		assertEquals(5, Main.solve("<<<<"));
+		assertEquals(5, Main.solve(">>>>"));
+		assertEquals(6, Main.solve(">>><>>>"));
+		assertEquals(5, Main.solve(">>><<>>>"));
+		assertEquals(6, Main.solve("><>>><>>>"));
+		assertEquals(9, Main.solve("<<<<<>>>>>>>>"));
+		assertEquals(2, Main.solve("<><><><><><><><><"));
+		assertEquals(10, Main.solve("<<<<<>>>>>>>><<<<<<<<<"));
+		assertEquals(10, Main.solve("<=<<<=<>=>=>==>=>>>=>=<=<<<==<==<=<=<=<><"));
+		assertEquals(10, Main.solve("<=<<<=<>=>=<>=>==<>=>=>>>=>=<==<<<==<==<=<==<=<><"));
+		assertEquals(3, Main.solve("<<>><>"));
+		assertEquals(3, Main.solve("<<>><><>"));
+		assertEquals(3, Main.solve("<><<>><><>"));
+		assertEquals(3, Main.solve("<<><>><><>"));
+		assertEquals(5, Main.solve("<<<<>>>><<>><<<<>><<>>>>"));
+		assertEquals(6, Main.solve("<<<<>>>><<>><<<<>><<>>>>>"));
+		assertEquals(3, Main.solve("<>><<>"));
+		assertEquals(5, Main.solve(">>>>"));
+		assertEquals(5, Main.solve(">>>><"));
+		assertEquals(5, Main.solve(">>>><<"));
+		assertEquals(5, Main.solve(">>>><<<"));
+		assertEquals(5, Main.solve(">>>><<<<"));
+		assertEquals(6, Main.solve(">>>><<<<<"));
+		assertEquals(6, Main.solve("<<><<><<><<"));
+		assertEquals(6, Main.solve(">><>><>><>>"));
+		assertEquals(15, Main.solve("<<<<<<<<<<>><<<<>><<<<>>>>>>>>>>>>>>"));
+		assertEquals(16, Main.solve("<<<<<<<<<<>><<<<>><<<<>>>>>>>>>>>>>>>"));
+		assertEquals(16, Main.solve("<<<<<<<<<<=>><<<<=>><<<<=>>>>>>>>>>>>>>>"));
+		assertEquals(16, Main.solve("<<<<=<<<<<<=>><<<<=>><<<<=>>>>>>>>>>>=>>>>"));
+	}
+
+}
