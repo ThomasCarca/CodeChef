f061ee91772a3cb01cd8d8378432b2b56f162da3
eisti <boulbestho@eisti.eu>
2018-02-23 15:31:35 +0100

    BUGCAL - Answer Accepted

---
+++ Practice/Beginner/BUGCAL/src/Main.java
@@ -0,0 +1,134 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	static int solve(int a, int b) {
+		int sum = a + b, sub = 0, mod = 10;
+		while (a > 0 && b > 0) {
+			if (a % 10 + b % 10 >= 10) {
+				sub += mod;
+			}
+			mod *= 10;
+			a /= 10;
+			b /= 10;
+		}
+		return sum - sub;
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		FastReader rdr = new FastReader();
+		PrintWriter stdout = new PrintWriter(System.out);
+		int T = rdr.nextInt();
+		while (T-- > 0) {
+			int a = rdr.nextInt(), b = rdr.nextInt();
+			stdout.println(solve(a, b));
+			
+		}
+		stdout.flush();
+		stdout.close();
+	}
+
+	//////////////////////////////////////////////////
+	////////////////// +--------+ ////////////////////
+	////////////////// | READER | ////////////////////
+	////////////////// +--------+ ////////////////////
+	//////////////////////////////////////////////////
+
+	/**
+	 * The Class FastReader.
+	 */
+	static class FastReader {
+
+		/** The br. */
+		BufferedReader br;
+
+		/** The st. */
+		StringTokenizer st;
+
+		/**
+		 * Instantiates a new fast reader.
+		 */
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		/**
+		 * Next.
+		 *
+		 * @return the string
+		 */
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		/**
+		 * Next int.
+		 *
+		 * @return the int
+		 */
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		/**
+		 * Next long.
+		 *
+		 * @return the long
+		 */
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		/**
+		 * Next double.
+		 *
+		 * @return the double
+		 */
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		/**
+		 * Next line.
+		 *
+		 * @return the string
+		 */
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
