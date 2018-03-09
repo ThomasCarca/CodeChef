f78990a8a17b2f027c64a12817954a6d5401cb14
eisti <boulbestho@eisti.eu>
2018-03-09 15:55:37 +0100

    CHEGLOVE - Answer Accepted

---
+++ Compete/MarchChallenge2018/CHEGLOVE/src/Main.java
@@ -0,0 +1,135 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Main.
+ */
+public class Main {
+
+	static String solve(int n, String hand, String glove) {
+		int[] handArray = Stream.of(hand.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		int[] gloveArray = Stream.of(glove.split("\\s+")).mapToInt(Integer::parseInt).toArray();
+		boolean front = true, back = true, both = false, none = false;
+		for (int i = 0, j = n - 1; i < gloveArray.length; i++, j--) {
+			front &= handArray[i] <= gloveArray[i];
+			back &= handArray[i] <= gloveArray[j];
+		}
+		both = front && back;
+		none = !front && !back;
+		return none ? "none" : both ? "both" : front ? "front" : "back";
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		FastReader rdr = new FastReader();
+		PrintWriter stdout = new PrintWriter(System.out);
+		int T = rdr.nextInt();
+		while (T-- > 0) {
+			int n = rdr.nextInt();
+			String hand = rdr.nextLine(), glove = rdr.nextLine();
+			stdout.println(solve(n, hand, glove));
+		}
+		stdout.flush();
+		stdout.close();
+	}
+
+	//////////////////////////////////////////////////
+	////////////////// +--------+ ////////////////////
+	////////////////// | READER | ////////////////////
+	////////////////// +--------+ ////////////////////
+	//////////////////////////////////////////////////
+
+	/**
+	 * The Class FastReader.
+	 */
+	static class FastReader {
+
+		/** The br. */
+		BufferedReader br;
+
+		/** The st. */
+		StringTokenizer st;
+
+		/**
+		 * Instantiates a new fast reader.
+		 */
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		/**
+		 * Next.
+		 *
+		 * @return the string
+		 */
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		/**
+		 * Next int.
+		 *
+		 * @return the int
+		 */
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		/**
+		 * Next long.
+		 *
+		 * @return the long
+		 */
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		/**
+		 * Next double.
+		 *
+		 * @return the double
+		 */
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		/**
+		 * Next line.
+		 *
+		 * @return the string
+		 */
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+
+}
