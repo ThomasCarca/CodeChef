648d337bb7e5fd0a8d49746fdba7e05c8e149c21
Thomas <boulbestho@eisti.eu>
2019-09-15 15:50:10 +0200

    RECTANGL - Answer Accepted

--- Practice/Beginner/RECTANGL/src/Main.java
+++ Practice/Beginner/RECTANGL/src/Main.java
@@ -2,6 +2,7 @@
 /*
  * Created by Thomas Boulbes
  */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,117 +10,122 @@ import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 // TODO: Auto-generated Javadoc
+
 /**
  * The Class Main.
  */
 public class Main {
 
-	static int solve(int n, int k, String line) {
-		return 0;
-	}
+    static String solve(int a, int b, int c, int d) {
+    	if (a==b) return c==d ? "YES" : "NO";
+    	if (a==c) return b==d ? "YES" : "NO";
+    	if (a==d) return b==c ? "YES" : "NO";
+    	return "NO";
+    }
 
-	/**
-	 * The main method.
-	 *
-	 * @param args
-	 *            the arguments
-	 * @throws IOException
-	 *             Signals that an I/O exception has occurred.
-	 */
-	public static void main(String[] args) throws IOException {
-		FastReader rdr = new FastReader();
-		PrintWriter stdout = new PrintWriter(System.out);
-		int T = rdr.nextInt();
-		while (T-- > 0) {
-			int n = rdr.nextInt(), k = rdr.nextInt();
-			String line = rdr.nextLine();
-			stdout.println(solve(n, k, line));
-		}
-		stdout.flush();
-		stdout.close();
-	}
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public static void main(String[] args) throws IOException {
+        FastReader rdr = new FastReader();
+        PrintWriter stdout = new PrintWriter(System.out);
+        int T = rdr.nextInt();
+        while (T-- > 0) {
+            int a = rdr.nextInt(), b = rdr.nextInt(), c = rdr.nextInt(), d = rdr.nextInt();
+            stdout.println(solve(a, b, c, d));
+        }
+        stdout.flush();
+        stdout.close();
+    }
 
-	//////////////////////////////////////////////////
-	////////////////// +--------+ ////////////////////
-	////////////////// | READER | ////////////////////
-	////////////////// +--------+ ////////////////////
-	//////////////////////////////////////////////////
+    //////////////////////////////////////////////////
+    ////////////////// +--------+ ////////////////////
+    ////////////////// | READER | ////////////////////
+    ////////////////// +--------+ ////////////////////
+    //////////////////////////////////////////////////
 
-	/**
-	 * The Class FastReader.
-	 */
-	static class FastReader {
+    /**
+     * The Class FastReader.
+     */
+    static class FastReader {
 
-		/** The br. */
-		BufferedReader br;
+        /**
+         * The br.
+         */
+        BufferedReader br;
 
-		/** The st. */
-		StringTokenizer st;
+        /**
+         * The st.
+         */
+        StringTokenizer st;
 
-		/**
-		 * Instantiates a new fast reader.
-		 */
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
+        /**
+         * Instantiates a new fast reader.
+         */
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
 
-		/**
-		 * Next.
-		 *
-		 * @return the string
-		 */
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
+        /**
+         * Next.
+         *
+         * @return the string
+         */
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
 
-		/**
-		 * Next int.
-		 *
-		 * @return the int
-		 */
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
+        /**
+         * Next int.
+         *
+         * @return the int
+         */
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
 
-		/**
-		 * Next long.
-		 *
-		 * @return the long
-		 */
-		long nextLong() {
-			return Long.parseLong(next());
-		}
+        /**
+         * Next long.
+         *
+         * @return the long
+         */
+        long nextLong() {
+            return Long.parseLong(next());
+        }
 
-		/**
-		 * Next double.
-		 *
-		 * @return the double
-		 */
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
+        /**
+         * Next double.
+         *
+         * @return the double
+         */
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
 
-		/**
-		 * Next line.
-		 *
-		 * @return the string
-		 */
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
+        /**
+         * Next line.
+         *
+         * @return the string
+         */
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 
 }
