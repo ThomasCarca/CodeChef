d4181a4acc60ad6c5d0d73a0d1739e03ad607dd9
Thomas <boulbestho@eisti.eu>
2019-09-16 14:54:55 +0200

    HORSES - Answer Accepted

---
+++ Practice/Easy/HORSES/src/Main.java
@@ -0,0 +1,139 @@
+
+/*
+ * Created by Thomas Boulbes
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+import java.util.stream.Stream;
+
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class Main.
+ */
+public class Main {
+
+    static int solve(Integer[] S) {
+        Arrays.sort(S, Collections.reverseOrder());
+        Integer minDifference = Integer.MAX_VALUE;
+        Integer difference;
+        for (int i = 0; i < S.length-1; i++) {
+            difference = S[i] - S[i+1];
+            minDifference = difference < minDifference ? difference : minDifference;
+        }
+        return minDifference;
+    }
+
+    /**
+     * The main method.
+     *
+     * @param args the arguments
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
+    public static void main(String[] args) throws IOException {
+        FastReader rdr = new FastReader();
+        PrintWriter stdout = new PrintWriter(System.out);
+        int T = rdr.nextInt();
+        while (T-- > 0) {
+            int N = rdr.nextInt();
+            Integer[] S = Stream.of(rdr.nextLine().split("\\s+")).map(Integer::valueOf).toArray(Integer[]::new);
+            stdout.println(solve(S));
+        }
+        stdout.flush();
+        stdout.close();
+    }
+
+    //////////////////////////////////////////////////
+    ////////////////// +--------+ ////////////////////
+    ////////////////// | READER | ////////////////////
+    ////////////////// +--------+ ////////////////////
+    //////////////////////////////////////////////////
+
+    /**
+     * The Class FastReader.
+     */
+    static class FastReader {
+
+        /**
+         * The br.
+         */
+        BufferedReader br;
+
+        /**
+         * The st.
+         */
+        StringTokenizer st;
+
+        /**
+         * Instantiates a new fast reader.
+         */
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        /**
+         * Next.
+         *
+         * @return the string
+         */
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        /**
+         * Next int.
+         *
+         * @return the int
+         */
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        /**
+         * Next long.
+         *
+         * @return the long
+         */
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        /**
+         * Next double.
+         *
+         * @return the double
+         */
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        /**
+         * Next line.
+         *
+         * @return the string
+         */
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+
+}
